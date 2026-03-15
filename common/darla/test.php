abb88c5b75cefdb92d70e9212fcec95673ee5871
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-03-15 18:36:36 -0400

    Create test.php

---
+++ common/darla/test.php
@@ -0,0 +1,5 @@
+<?php
+// Example PHP stub that takes a document named test.xml and displays it as HTML5.
+require $_SERVER["DOCUMENT_ROOT"] . '/darla-parser.php';
+translate_to_html('test.xml');
+?>
